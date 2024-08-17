ca063bceaa2fdefb56b77e459588437c42be2e7d
Le Anh Duc <ducvui2003@gmail.com>
2024-08-17 15:31:07 +0700

    feat: add prettier, eslint and editorconfig

--- src/components/card/product/ProductHomeCard.tsx
+++ src/components/card/product/ProductHomeCard.tsx
@@ -13,34 +13,34 @@ import {ProductHomeCardType} from "./type/productHomeCard.type";
 
 
 function ProductHomeCard({product: {name, rating, id, basePrice, salePrice, image}}: ProductHomeCardType) {
-    const theme: ThemeType = useSelector((state: RootState) => state.themeState.theme);
-    const styles = makeStyled(theme);
-    return (
-        <View style={styles.container}>
-            <Image style={styles.thumbnail}
-                   src={"https://plus.unsplash.com/premium_photo-1668618295237-f1d8666812c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
-            <View style={styles.content}>
-                <Text>{name}</Text>
-                <Text>{rating}</Text>
-                <Text>{basePrice}</Text>
-                <Text>{salePrice}</Text>
-            </View>
-        </View>
-    );
+	const theme: ThemeType = useSelector((state: RootState) => state.themeState.theme);
+	const styles = makeStyled(theme);
+	return (
+		<View style={styles.container}>
+			<Image style={styles.thumbnail}
+				   src={"https://plus.unsplash.com/premium_photo-1668618295237-f1d8666812c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+			<View style={styles.content}>
+				<Text>{name}</Text>
+				<Text>{rating}</Text>
+				<Text>{basePrice}</Text>
+				<Text>{salePrice}</Text>
+			</View>
+		</View>
+	);
 }
 
 const makeStyled = (theme: ThemeType) => StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        padding: 8,
-        borderRadius: 12,
-    },
-    thumbnail: {
-        width: "100%",
-        height: 100,
-        borderRadius: 8,
-    },
-    content: {}
+	container: {
+		backgroundColor: "white",
+		padding: 8,
+		borderRadius: 12,
+	},
+	thumbnail: {
+		width: "100%",
+		height: 100,
+		borderRadius: 8,
+	},
+	content: {}
 });
 
 export default ProductHomeCard;
