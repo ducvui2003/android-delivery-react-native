2f3dbcdc40aca10a51c418f063d3d26dbf0b9d0b
Le Anh Duc <ducvui2003@gmail.com>
2024-08-19 16:15:59 +0700

    feat: add GradientIcnSvg from LamNguyen

---
+++ src/components/grandientIconSvg/GradientIconSvg.tsx
@@ -0,0 +1,22 @@
+/**
+ * Author: Nguyen Dinh Lam
+ * Email: kiminonawa1305@gmail.com
+ * Phone number: +84 855354919
+ * Create at: 9:19 AM - 18/08/2024
+ * User: lam-nguyen
+ **/
+
+import React from "react";
+import { Platform } from "react-native";
+import GradientProps from "./type/gradient.type";
+
+let LinearGradient: React.ComponentType<GradientProps>;
+if (Platform.OS === "android" || Platform.OS === "ios")
+	LinearGradient = require("./GradientIconSvg.android").default;
+else if (Platform.OS === "web") LinearGradient = require("./GradientIconSvg.web").default;
+
+function GradientIconSvg(props: GradientProps) {
+	return <LinearGradient {...props} />;
+}
+
+export default GradientIconSvg;
