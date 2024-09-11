2f32561ff758522d847e2fae916d2b22e6d21128
Nguyễn Đình Lam <21130416@st.hcmuaf.edu.vn>
2024-09-11 10:14:43 +0700

    feat: Hoàn thành component Modal.tsx

---
+++ src/components/modal/type/modal.props.ts
@@ -0,0 +1,23 @@
+/**
+ * Author: Nguyen Dinh Lam
+ * Email: kiminonawa1305@gmail.com
+ * Phone number: +84 855354919
+ * Create at: 8:04 PM - 10/09/2024
+ * User: lam-nguyen
+ **/
+import { ReactNode } from "react";
+import { StyleProp, ViewStyle } from "react-native";
+import { DimensionValue } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
+
+type ModalProps = {
+	children: ReactNode;
+	contentStyle?: StyleProp<ViewStyle>;
+	containerStyle?: StyleProp<ViewStyle>;
+	active: boolean;
+	backgroundColor?: string;
+	opacityBackground?: number;
+	width?: DimensionValue;
+	durationAnimation?: number;
+};
+
+export default ModalProps;
